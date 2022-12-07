8b576fe2d644d5b9fe38095f156fe84f873a7bdd
Peter Boomsma <peter@cleanpixel.nl>
2022-12-08 00:36:20 +0100

    solved part 2 of day 3

--- day3/index.ts
+++ day3/index.ts
@@ -2,23 +2,52 @@ import * as fs from 'fs';
 const data = fs.readFileSync('day3/items.txt','utf8')
 
 let items: Array<string>
-let priorityItemsValue: number = 0;
 
 function sanitizeData(data: string) {
   items = data.split('\r\n').map((line) => {
     return line;
   })
 
-  findDuplicateItem(items)
+  priorityItemsValue(items)
+  badgesValue(items)
 }
 
-function findDuplicateItem(items: Array<string>) {
+function priorityItemsValue(items: Array<string>) {
+  let priorityItemsValue = 0;
+
   items.map((item) => {
     const compartmentLength = item.length / 2
     const firstCompartment = item.slice(0, compartmentLength);
     const secondCompartment = item.slice(compartmentLength, compartmentLength * 2);
     const duplicateItem = [ ...firstCompartment].filter((item) => secondCompartment.includes(item) ? item : null)[0]
     priorityItemsValue += getPriority(duplicateItem)
+  })
+  console.log(priorityItemsValue)
+}
+
+function badgesValue(items: Array<string>) {
+  const elfGroup = 3;
+  let priorityItemsValue = 0;
+  let allElfGroups: Array<Array<string>> = []
+  let groupOfElves: Array<string> = [];
+
+  items.map((elf, i) => {
+    i++
+    groupOfElves.push(elf)
+    if (i % elfGroup === 0) {
+      allElfGroups.push(groupOfElves)
+      groupOfElves = []
+    }
+  })
+
+  allElfGroups.map((elfGroup) => {
+    const badge = elfGroup.map((elf) => {
+      let letter = [...elf].filter((letter) => {
+        return elfGroup[0].includes(letter) && elfGroup[1].includes(letter) && elfGroup[2].includes(letter) ? letter : null
+      })
+      return letter[0];
+    })
+    priorityItemsValue += getPriority(badge[0])
   })
   console.log(priorityItemsValue)
 }
