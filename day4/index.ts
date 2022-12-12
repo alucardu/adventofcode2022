066a94c3cbffdd042acdc44d1cd0b7fb39631b97
Peter Boomsma <peter@klippa.com>
2022-12-12 21:43:37 +0100

    solved part 1 and part 2

--- day4/index.ts
+++ day4/index.ts
@@ -1,2 +1,18 @@
 import * as fs from 'fs';
 const data = fs.readFileSync('day4/sections.txt','utf8')
+
+function sanitizeData(data: string): void {
+	const sanitizedData = data.split('\n').map((line) => line.split(',')).map((line) => line.map((line) => line.split('-').map(Number)))
+	const partOverlappingSections = sanitizedData.filter((section) => {
+		const [[firstStart, firstEnd], [secondStart, secondEnd]] = section;
+		if ((firstStart <= secondEnd && firstEnd >= secondStart) || (secondStart <= firstEnd && secondEnd >= firstStart)) return section
+	})
+
+	const overlappingSections = sanitizedData.filter((section) => {
+		const [[firstStart, firstEnd], [secondStart, secondEnd]] = section;
+		if ((firstStart <= secondStart && firstEnd >= secondEnd) || (secondStart <= firstStart && secondEnd >= firstEnd)) return section
+	})
+	console.log(overlappingSections.length, partOverlappingSections.length)
+}
+
+sanitizeData(data)
