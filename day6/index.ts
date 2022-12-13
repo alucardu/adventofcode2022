a456f333ee08100c2ac637ed51c446346455d38c
Peter Boomsma <peter@klippa.com>
2022-12-14 00:35:17 +0100

    finished part 2

--- day6/index.ts
+++ day6/index.ts
@@ -3,14 +3,14 @@ const data = fs.readFileSync('day6/input.txt','utf8')
 
 function parseInput(data: string): void {
 	for (let index = 0; index < data.length; index++) {
-		const something = [...data].slice(index, index + 4)
+		const something = [...data].slice(index, index + 14)
 
 		const x = something.some((item, index) => {
 			return something.indexOf(item) !== index
 		})
 
 		if (!x) {
-			console.log('UNDEFINED', index + 4)
+			console.log('UNDEFINED', index + 14)
 			return
 		}
 
